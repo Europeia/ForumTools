c2dec33610e2d2727550a2df1d46fc1da3013976
ThatDarcGuy <darc@outlook.com>
2018-11-19 23:34:33 -0800

    Adding Law Index (as opposed to treaty index

--- forumtools/forum.js
+++ forumtools/forum.js
@@ -1,23 +1,39 @@
-var treaties = [];
+function constructLawUrl(forum)
+{
+    return 'http://www.europeians.com/forum/index.php?forums/' + forum + '/';
+}
 
-function fetchAndParseTreaties() {
-  var url =
-    'http://www.europeians.com/forum/index.php?forums/treaty-law.60031/index.rss';
+lawFetchData = [
+  {
+    name: 'Law Index',
+    url: constructLawUrl('treaty-law.60031'),
+    htmlOutput: ''
+  },
+  {
+    name: 'Treaty Index',
+    url: constructLawUrl('alphabetical-list-of-acts-and-executive-orders.60032'),
+    htmlOutput: ''
+  }
+];
 
-  $.ajax(url, {
+function fetchAndParseLawIndex(data) {
+  var feed = data.url + 'index.rss';
+
+  $.ajax(feed, {
     accepts: {
       xml: 'application/rss+xml'
     },
     dataType: 'xml',
-    success: function(data) {
-      $(data)
+    success: function(response) {
+      $(response)
         .find('item')
         .each(function() {
           var el = $(this);
           var item = '<li><span class="infoentry"><a href="';
           item += el.find('link').text();
           item += '">' + el.find('title').text() + '</a></span></li>';
-          treaties.push(item);
+
+          data.htmlOutput += item;
         });
     }
   });
