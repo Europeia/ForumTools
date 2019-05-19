5b8b023c26540ab2e0a7035edbd980ed3f3e9330
Darcness <darc@outlook.com>
2019-05-18 23:26:23 -0700

    Fixing the other URL (banner image) (#10)

--- forumtools/news.js
+++ forumtools/news.js
@@ -2,7 +2,7 @@ newsFetchData = {
   name: "Europeia Broadcasting Corporation",
   url: constructForumUrl("4010143"),
   htmlOutput:
-    '<ul><li><a href="/forum/index.php?forums/4010143"><img src="https://static.europeians.com/img_repo/EBC_letterhead.png"></a></li>'
+    '<ul><li><a href="index.php?forums/4010143"><img src="https://static.europeians.com/img_repo/EBC_letterhead.png"></a></li>'
 };
 
 function compareNewsItems(a, b) {
